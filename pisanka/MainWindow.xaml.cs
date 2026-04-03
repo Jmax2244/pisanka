b4505a508db14deff36cbd59800472812756420b
Oliwier <Oliwier@DESKTOP-HSI905G>
2026-04-03 23:18:54 +0200

    1

--- pisanka/MainWindow.xaml.cs
+++ pisanka/MainWindow.xaml.cs
@@ -1,25 +1,46 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace pisanka
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        const int SIZE = 10;
+        Border[,] cells = new Border[SIZE, SIZE];
+
         public MainWindow()
         {
             InitializeComponent();
+            InitGrid();
+        }
+
+        void InitGrid()
+        {
+            for (int i = 0; i < SIZE; i++)
+            {
+                GameGrid.RowDefinitions.Add(new RowDefinition());
+                GameGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            for (int y = 0; y < SIZE; y++)
+            {
+                for (int x = 0; x < SIZE; x++)
+                {
+                    Border b = new Border
+                    {
+                        BorderBrush = Brushes.Black,
+                        BorderThickness = new Thickness(1),
+                        Background = Brushes.White
+                    };
+
+                    Grid.SetRow(b, y);
+                    Grid.SetColumn(b, x);
+                    GameGrid.Children.Add(b);
+
+                    cells[x, y] = b;
+                }
+            }
         }
     }
 }
-
